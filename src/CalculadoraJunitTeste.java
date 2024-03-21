5d33b37a771d4e63c56305cdd7d63f68784f7b69
Nathan <naa.limaa@hotmail.com>
2024-03-21 17:33:54 -0300

    Adicionado cenários de teste de Soma, Subtração, Divisão e Multiplicação no JUNIT.

--- src/CalculadoraJunitTeste.java
+++ src/CalculadoraJunitTeste.java
@@ -11,79 +11,45 @@ public class CalculadoraJunitTeste {
         Calculadora calc = new Calculadora();
 
 
-        //Cenário de Teste de Soma 1: A soma correta é 10, porém no teste ele está esperando 12, o teste vai dar erro.
+        //Cenário de Teste de Soma 1: A soma correta é 10, porém no teste ele está esperando 12, irá falhar o teste.
         int somar = calc.soma(3, 7);
         System.out.println("Soma: \n" + somar);
         assertEquals(12, somar);
 
-        //Cenário de Teste de Soma 2: A soma correta é 7 e no teste ele está esperando 7, o teste vai passado com sucesso.
+        //Cenário de Teste de Soma 2: A soma correta é 7 e no teste ele está esperando 7, o teste vai ser executado com sucesso.
         somar = calc.soma(2, 5);
         System.out.println("Soma: \n" + somar);
         assertEquals(7, somar);
 
-        //Cenário de Teste de Subtração 1: Subtração de dois números.
+        //Cenário de Teste de Subtração 1: A subtração correta é 10, no teste ele está esperando 10, o teste vai ser executado com sucesso.
         int subtracao = calc.subtracao(15, 5);
         System.out.println("Subtração: \n" + subtracao);
+        assertEquals(10, subtracao);
 
-        //Cenário de Teste de Subtração 2: Subtração de dois números, sendo um número negativo.
-        subtracao = calc.subtracao(-15, 15);
+        //Cenário de Teste de Subtração 2: A subtração correta é 0, porém no teste ele está esperando 2, irá falhar o teste.
+        subtracao = calc.subtracao(15, 15);
         System.out.println(subtracao);
+        assertEquals(2, subtracao);
 
-        //Cenário de Teste de Multiplicação 1: Multiplicação de dois números.
-        int multiplicacao = calc.multiplicacao(5, 5);
+        //Cenário de Teste de Multiplicação 1: A multiplicação correta é 81, no teste ele está esperando 81, o teste vai ser executado com sucesso.
+        int multiplicacao = calc.multiplicacao(9, 9);
         System.out.println("Multiplicação: \n" + multiplicacao);
+        assertEquals(81, multiplicacao);
 
-        //Cenário de Teste de Multiplicação 2: Multiplicação de dois números.
-        multiplicacao = calc.multiplicacao(3, 2);
-        System.out.println(multiplicacao);
+        //Cenário de Teste de Multiplicação 2: A multiplicação correta é 25, porém no teste ele está esperando 30, irá falhar o teste.
+        multiplicacao = calc.multiplicacao(5, 5);
+        assertEquals(30, multiplicacao);
 
-        //Cenário de Teste de Multiplicação 3: Multiplicação de dois números.
-        multiplicacao = calc.multiplicacao(8, 3);
-        System.out.println(multiplicacao);
-
-        //Cenário de Teste de Multiplicação 4: Multiplicação de dois números, sendo um 0.
-        multiplicacao = calc.multiplicacao(9, 0);
-        System.out.println(multiplicacao);
-
-        //Cenário de Teste de Multiplicação 5: Multiplicação de dois números, sendo um negativo.
-        multiplicacao = calc.multiplicacao(7, -6);
-        System.out.println(multiplicacao);
-
-        //Cenário de Teste de Multiplicação 6: Multiplicação de dois números, sendo dois negativos.
-        multiplicacao = calc.multiplicacao(-10, -6);
-        System.out.println(multiplicacao);
-
-        //Cenário de Teste de Multiplicação 7: Multiplicação de dois números.
-        multiplicacao = calc.multiplicacao(7, 12);
-        System.out.println(multiplicacao);
-
-        //Cenário de Teste de Divisão 1: Divisão de dois números.
-        int divisao = calc.divisao(10, 2);
+        //Cenário de Teste de Divisão 1: A divisão correta é 8, porém no teste ele está esperando 10, irá falhar o teste.
+        int divisao = calc.divisao(80, 10);
         System.out.println("Divisão: \n" + divisao);
+        assertEquals(10, divisao);
 
-        //Cenário de Teste de Divisão 2: Divisão de dois números.
-        divisao = calc.divisao(20, 3);
+        //Cenário de Teste de Divisão 2: A multiplicação correta é 5, no teste ele está esperando 5, o teste vai ser executado com sucesso.
+        divisao = calc.divisao(25, 5);
         System.out.println(divisao);
+        assertEquals(5, divisao);
 
-        //Cenário de Teste de Divisão 3: Divisão de dois números, sendo um negativo.
-        divisao = calc.divisao(2, -2);
-        System.out.println(divisao);
-
-        //Cenário de Teste de Divisão 4: Divisão de dois números, sendo dois negativos.
-        divisao = calc.divisao(-5, -5);
-        System.out.println(divisao);
-
-        //Cenário de Teste de Divisão 5: Divisão de dois números.
-        divisao = calc.divisao(50, 20);
-        System.out.println(divisao);
-
-        //Cenário de Teste de Divisão 6: Divisão de dois números.
-        divisao = calc.divisao(70, 7);
-        System.out.println(divisao);
-
-        //Cenário de Teste de Divisão 7: Divisão de dois números.
-        divisao = calc.divisao(20, 9);
-        System.out.println(divisao);
 
     }
 
